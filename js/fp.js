ef18aba43304082ffc6d6627427ce496c2711569
haolu253 <luchihao253@gmail.com>
2022-10-17 17:46:16 +0700

    update

--- js/fp.js
+++ js/fp.js
@@ -4,9 +4,12 @@ new fullpage('#fullpage', {
     fitToSection: true,
     navigation: true,
     navigationPosition: 'left',
-    anchors: ['s1', 's2', 's3', 's4'],
-    responsiveSlides: true,
-    // afterRender: function () {
-    //     $('video').get(0).play();
-    // }
+    anchors: ['s1', 's2', 's3', 's4', 's5']
 })
+
+// $('.section-content').addClass('d-none')vaguely
+
+// for (let i = 1; i <= 5; i++) {
+//     if ($('.fp-section.s'+i).hasClass('.active')) {
+//     }
+// }
