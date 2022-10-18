ddede741427342ca84f7fa2df99452f2fde1d3ee
haolu253 <luchihao253@gmail.com>
2022-10-18 11:22:12 +0700

    fix fp

--- js/fp.js
+++ js/fp.js
@@ -1,4 +1,4 @@
-new fullpage('#fullpage', {
+$('#fullpage').fullpage({
     autoScrolling: true,
     dragAndMove: true,
     fitToSection: true,
@@ -6,10 +6,3 @@ new fullpage('#fullpage', {
     navigationPosition: 'left',
     anchors: ['s1', 's2', 's3', 's4', 's5']
 })
-
-// $('.section-content').addClass('d-none')vaguely
-
-// for (let i = 1; i <= 5; i++) {
-//     if ($('.fp-section.s'+i).hasClass('.active')) {
-//     }
-// }
