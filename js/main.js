55bf3cd81739d53a9822f37aec79bb1c81c86069
haolu253 <luchihao253@gmail.com>
2022-11-23 10:32:23 +0700

    update animation

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $('.load-animation').addClass('load-rotate');
     setTimeout(function () {
         $('.load-animation').addClass('d-none');
-    }, 1001);
+    }, 1501);
     setTimeout(function () {
         $('.load-background .red').addClass('load-left');
         $('.load-background .purple').addClass('load-right');
-    }, 1001);
+    }, 1501);
     setTimeout(function () {
         $('.load-background').addClass('d-none');
-    }, 2001);
+    }, 2501);
 })
