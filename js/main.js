122911ac0947c72649a274244744c36aeb227efe
haolu253 <luchihao253@gmail.com>
2022-12-02 17:50:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('.menu-item').on('mouseenter', function () {
         var hoverEff = $(this).find('.menu-hover');
-        hoverEff.addClass('hover-eff').css('height', '100%');
+        hoverEff.addClass('hover-eff').removeClass('hover-noeff').css('height', '100%');
     });
 
     $('.menu-item').on('mouseleave', function () {
         var hoverEff = $(this).find('.menu-hover');
-        hoverEff.removeClass('hover-eff').css('height', '0%');
+        hoverEff.removeClass('hover-eff').addClass('hover-noeff').css('height', '0%');
     });
 })
