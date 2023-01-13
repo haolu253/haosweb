c730046d21468b98c536b61d9ef4e8c567f72baf
haolu253 <luchihao253@gmail.com>
2023-01-13 09:46:32 +0700

    firework

--- js/main.js
+++ js/main.js
@@ -18,4 +18,27 @@ $(document).ready(function () {
             $('.small-menu').addClass('is-active')
         }
     })
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
 })
+
