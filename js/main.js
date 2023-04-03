992da49562358f20e9d639d135f8b029e1abc802
haolu253 <luchihao253@gmail.com>
2023-04-03 15:14:54 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,44 +1,19 @@
-$(document).ready(function () {
-    $('.menu-item').on('mouseenter', function () {
-        var hoverEff = $(this).find('.menu-hover');
-        hoverEff.addClass('hover-eff').removeClass('hover-noeff').css('height', '100%');
-    });
-
-    $('.menu-item').on('mouseleave', function () {
-        var hoverEff = $(this).find('.menu-hover');
-        hoverEff.removeClass('hover-eff').addClass('hover-noeff').css('height', '0%');
-    });
-
-    $('.float-menu').on('click', function () {
-        if($(this).hasClass('is-active')) {
-            $(this).removeClass('is-active');
-            $('.small-menu').removeClass('is-active')
-        } else {
-            $(this).addClass('is-active');
-            $('.small-menu').addClass('is-active')
-        }
-    })
-
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      });
+$('.home-section-show').on('click', function () {
+  $('.section').addClass('hidden-section').removeClass('show-section');
+  $('.home-section').addClass('show-section').removeClass('hidden-section');
 })
 
+$('.about-section-show').on('click', function () {
+  $('.section').addClass('hidden-section').removeClass('show-section');
+  $('.about-section').addClass('show-section').removeClass('hidden-section');
+})
+
+$('.projects-section-show').on('click', function () {
+  $('.section').addClass('hidden-section').removeClass('show-section');
+  $('.projects-section').addClass('show-section').removeClass('hidden-section');
+})
+
+$('.contact-section-show').on('click', function () {
+  $('.section').addClass('hidden-section').removeClass('show-section');
+  $('.contact-section').addClass('show-section').removeClass('hidden-section');
+})
