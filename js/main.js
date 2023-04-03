89917e6f37162ee5de0d153903bee2386cf5a89d
haolu253 <luchihao253@gmail.com>
2023-04-03 15:24:09 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 $('.home-section-show').on('click', function () {
-  $('.section').addClass('hidden-section').removeClass('show-section');
-  $('.home-section').addClass('show-section').removeClass('hidden-section');
+  $('.section').addClass('section-fade');
+  setTimeout(() => {
+    $('.section').addClass('hidden-section').removeClass('show-section section-fade');
+    $('.home-section').addClass('show-section').removeClass('hidden-section section-fade');
+  }, 300);
 })
 
 $('.about-section-show').on('click', function () {
-  $('.section').addClass('hidden-section').removeClass('show-section');
-  $('.about-section').addClass('show-section').removeClass('hidden-section');
+  $('.section').addClass('section-fade');
+  setTimeout(() => {
+    $('.section').addClass('hidden-section').removeClass('show-section section-fade');
+    $('.about-section').addClass('show-section').removeClass('hidden-section section-fade');
+  }, 300);
 })
 
 $('.projects-section-show').on('click', function () {
-  $('.section').addClass('hidden-section').removeClass('show-section');
-  $('.projects-section').addClass('show-section').removeClass('hidden-section');
+  $('.section').addClass('section-fade');
+  setTimeout(() => {
+    $('.section').addClass('hidden-section').removeClass('show-section section-fade');
+    $('.projects-section').addClass('show-section').removeClass('hidden-section section-fade');
+  }, 300);
 })
 
 $('.contact-section-show').on('click', function () {
-  $('.section').addClass('hidden-section').removeClass('show-section');
-  $('.contact-section').addClass('show-section').removeClass('hidden-section');
+  $('.section').addClass('section-fade');
+  setTimeout(() => {
+    $('.section').addClass('hidden-section').removeClass('show-section section-fade');
+    $('.contact-section').addClass('show-section').removeClass('hidden-section section-fade');
+  }, 300);
 })
