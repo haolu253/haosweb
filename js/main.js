6015376c385647ff83914fbcd56ba2981f268b91
haolu253 <luchihao253@gmail.com>
2023-10-11 11:53:43 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     setTimeout(function() {
         $('.mam-fade').fadeOut(1000);
+        $('.mam-logo').removeClass('d-none');
+        $('.mam-logo-gif').addClass('d-none');
     }
     , 5000);
 });
